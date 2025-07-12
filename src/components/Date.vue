451849868e1bda179f75b2dca82e618f097bef22
Amir hossein nazeri <123809170+amirnazry@users.noreply.github.com>
2025-07-12 22:57:27 +0330

    Add files via upload

--- src/components/Date.vue
+++ src/components/Date.vue
@@ -1,1 +1,31 @@
+<template>
+  <div class="date">
+    امروز: {{ today }}
+  </div>
+</template>
 
+<script>
+export default {
+  data() {
+    return {
+      today: ''
+    }
+  },
+  mounted() {
+    const now = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour :'numeric',minute :'numeric',second : 'numeric' };
+    this.today = now.toLocaleDateString('fa-IR', options); // تقویم شمسی
+  }
+}
+</script>
+
+<style>
+.date {
+  font-weight: bold;
+  text-align: center;
+  margin: 0;
+  color: #4CAF50;
+}
+
+
+</style>
---
+++ src/components/FilterNav.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="filter-nav">
+        <button @click="updatefilter('all')" :class="{active : current === 'all'}">دیدن همه</button>
+        <button @click="updatefilter('completed')" :class="{active : current === 'completed'}">تکمیل شده ها</button>
+        <button @click="updatefilter('ongiong')" :class="{active : current === 'ongiong'}">تکمیل نشده ها</button>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    props : ['current'],
+    methods:{
+        updatefilter(by){
+                this.$emit('filterchage', by)
+        }
+    }
+}
+</script>
+
+<style>
+.filter-nav {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  margin-bottom: 20px;
+  flex-wrap: wrap;
+}
+
+.filter-nav button {
+  background: none;
+  border: none;
+  color: #657C6A;
+  font-family: 'vazir';
+  margin: 0 5px;
+  cursor: pointer;
+  padding: 7px 12px;
+  border-radius: 10px;
+}
+
+.filter-nav button.active {
+  background: #B0DB9C;
+}
+
+</style>
