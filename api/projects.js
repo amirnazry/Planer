6e23303a59d0656d8b010f706e12963a3b096961
Amir hossein nazeri <123809170+amirnazry@users.noreply.github.com>
2025-07-13 12:26:24 +0330

    Update projects.js

--- api/projects.js
+++ api/projects.js
@@ -5,6 +5,5 @@ export default function handler(req, res) {
   const filePath = path.join(process.cwd(), 'data', 'db.json')
   const fileData = fs.readFileSync(filePath, 'utf8')
   const data = JSON.parse(fileData)
-
   res.status(200).json(data.projects)
 }
