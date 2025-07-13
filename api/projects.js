d8f3afe675c5d848634bedb6c0ef1946f42e3b4e
Amir hossein nazeri <123809170+amirnazry@users.noreply.github.com>
2025-07-13 12:42:48 +0330

    Delete api/projects.js

--- api/projects.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-import path from 'path'
-
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'data', 'db.json')
-  const fileData = fs.readFileSync(filePath, 'utf8')
-  const data = JSON.parse(fileData)
-  res.status(200).json(data.projects)
-}
