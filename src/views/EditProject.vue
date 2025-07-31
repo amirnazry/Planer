edaf4caaf896bb2ec2c7a523ce1047a933c8452d
Amir hossein nazeri <123809170+amirnazry@users.noreply.github.com>
2025-07-31 12:46:31 +0330

    Update EditProject.vue

--- src/views/EditProject.vue
+++ src/views/EditProject.vue
@@ -24,7 +24,7 @@ export default {
             title : '',
             details : '',
             day : '',
-            uri : '/api/projects/' + this.id
+            uri : 'http://localhost:3000/projects/' + this.id
         }
     },
     mounted(){
